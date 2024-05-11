e00ca705a0211f27771b28bf7ce6c15b5ba5558d
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-05-11 12:08:54 +0100

    changed 5 files

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:food_delivery_app/layout.dart';
 import 'package:food_delivery_app/widgets/settings_page/logout_widget.dart';
+import '/widgets/settings_page/settings_custom_tile.dart';
 import '/widgets/settings_page/dark_mode_list_tile.dart';
 import '/widgets/settings_page/settings_header.dart';
 import '/widgets/settings_page/settings_container.dart';
@@ -10,41 +11,128 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
       children: [
         const SettingsHeader(),
-        SettingsContainer(
-          heading: "General",
-          child: GestureDetector(
-            onTap: () {},
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
+        Expanded(
+          child: ListView(
+            children: const [
+              SettingsContainer(
+                heading: "General",
+                child: Column(
+                  children: [
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Profile',
+                      icon: Icons.person,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'My Address',
+                      icon: Icons.location_city_outlined,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Language',
+                      icon: Icons.language,
+                      hasBorder: false,
+                    ),
+                  ],
+                ),
               ),
-              decoration: BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                color: Theme.of(context).colorScheme.primary,
-              ))),
-              child: const Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.groups_sharp,
-                  ),
-                  SizedBox(width: 15),
-                  Text("Refer & Earn"),
-                ],
+              SettingsContainer(
+                heading: "Promotional Activity",
+                child: Column(
+                  children: [
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Coupon',
+                      icon: Icons.book_online_outlined,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Loyalty Points',
+                      icon: Icons.stars_sharp,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'My Wallet',
+                      icon: Icons.wallet,
+                      hasBorder: false,
+                    ),
+                  ],
+                ),
               ),
-            ),
+              SettingsContainer(
+                heading: "Earnings",
+                child: Column(
+                  children: [
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Refer & Earn',
+                      icon: Icons.group_add_sharp,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Join as a Delivery Man',
+                      icon: Icons.motorcycle_sharp,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Become a Vendor',
+                      icon: Icons.language,
+                      hasBorder: false,
+                    ),
+                  ],
+                ),
+              ),
+              SettingsContainer(
+                heading: "Help & Support",
+                child: Column(
+                  children: [
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Live Chat',
+                      icon: Icons.support_agent,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Help & support',
+                      icon: Icons.phone,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'About Us',
+                      icon: Icons.info,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Terms & Conditions',
+                      icon: Icons.menu_book_rounded,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Privacy Policy',
+                      icon: Icons.menu_book_rounded,
+                    ),
+                    SettingsCustomTile(
+                      moveTo: Layout(),
+                      label: 'Refund Policy',
+                      icon: Icons.currency_exchange,
+                      hasBorder: false,
+                    ),
+                  ],
+                ),
+              ),
+              SettingsContainer(
+                heading: "Theme",
+                child: DarkModeListTile(),
+              ),
+              SizedBox(height: 20),
+              LogoutWidget(),
+            ],
           ),
         ),
-        const SettingsContainer(
-          heading: "Theme",
-          child: DarkModeListTile(),
-        ),
-        const SizedBox(height: 20),
-        const LogoutWidget(),
       ],
     );
   }
